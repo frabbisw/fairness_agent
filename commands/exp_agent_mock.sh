8556d2c8ffc1118938b0201b74232b681d7d51f1
Fazle Rabbi <frduiit@gmail.com>
2026-02-22 04:31:21 -0500

    Update exp_agent_mock.sh

--- commands/exp_agent_mock.sh
+++ commands/exp_agent_mock.sh
@@ -66,6 +66,7 @@ echo "===================="
 
 
 #reviewer agent
+cd "$CURRENT_DIR""/../generate_code" || exit
 python reviewer.py "$DATA_PATH" "$MODEL_DIR"/response/developer "$MODEL_DIR"/response/reviewer "$SAMPLING" "$TEMPERATURE" "$PROMPT_STYLE" "$MODEL_NAME" "$MODEL_DIR""/test_result/developer/bias_info_files"
 
 
