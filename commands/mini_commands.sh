8a5d5887ccc7351db6b8662dba12494721684b4d
Fazle Rabbi <frduiit@gmail.com>
2026-02-22 01:38:56 -0500

    Update mini_commands.sh

--- commands/mini_commands.sh
+++ commands/mini_commands.sh
@@ -1,2 +1,2 @@
-bash exp_agent_mock.sh 2 1 agent_developer "$(pwd)""/../dataset/prompts_sample.jsonl" "$(pwd)""/../outputs/agentic/""gpt""1""default" "gpt"
+bash exp_agent_mock.sh 2 1 agent_developer "$(pwd)""/../dataset/prompts_sample.jsonl" "$(pwd)""/../outputs/agentic/""gpt""1""default" "gpt" 3
 ##bash gen_and_exp.sh samples temp data_path out_folder model_name
