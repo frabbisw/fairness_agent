9475f6c462d8d2c7a23a9844a2d2ade8a4caff49
Fazle Rabbi <frduiit@gmail.com>
2026-02-22 05:37:53 -0500

    Update exp_agent_mock.sh

--- commands/exp_agent_mock.sh
+++ commands/exp_agent_mock.sh
@@ -30,12 +30,13 @@ echo count_bias_leaning.py "$MODEL_DIR"
 echo "===================="
 
 
-# # Delete the previous result files
-# rm -rf "$MODEL_DIR""/test_result/developer"
 
 # #generate and save response/developers from model
 # cd "$CURRENT_DIR""/../generate_code" || exit
 # # python developer.py "$DATA_PATH" "$MODEL_DIR"/response/developer "$SAMPLING" "$TEMPERATURE" "$PROMPT_STYLE" "$MODEL_NAME"
+
+# # Delete the previous result files
+# rm -rf "$MODEL_DIR""/test_result/developer"
 
 # #run test suits
 # cd "$CURRENT_DIR""/../fairness_test/test_suites/" || exit
@@ -69,4 +70,34 @@ cd "$CURRENT_DIR""/../generate_code" || exit
 # python reviewer.py "$DATA_PATH" "$MODEL_DIR"/response/developer "$MODEL_DIR"/response/reviewer "$SAMPLING" "$TEMPERATURE" agent_reviewer "$MODEL_NAME" "$MODEL_DIR""/test_result/developer/bias_info_files"
 
 #repair agent
-python repairer.py "$DATA_PATH" "$MODEL_DIR"/response/developer "$MODEL_DIR"/response/reviewer "$MODEL_DIR"/response/repairer "$SAMPLING" "$TEMPERATURE" agent_repair "$MODEL_NAME"
+# python repairer.py "$DATA_PATH" "$MODEL_DIR"/response/developer "$MODEL_DIR"/response/reviewer "$MODEL_DIR"/response/repairer "$SAMPLING" "$TEMPERATURE" agent_repair "$MODEL_NAME"
+
+# Delete the previous result files
+rm -rf "$MODEL_DIR""/test_result/repairer"
+
+#run test suits
+cd "$CURRENT_DIR""/../fairness_test/test_suites/" || exit
+
+BASE_DIR="$MODEL_DIR""/response/repairer"
+LOG_DIR="$MODEL_DIR""/test_result/repairer/log_files"
+REPORT_BASE_DIR="$MODEL_DIR""/test_result/repairer/inconsistency_files"
+
+cp config_template.py config.py
+sed -i "s|##PATH##TO##RESPONSE##|$BASE_DIR|g" config.py
+sed -i "s|##PATH##TO##LOG##FILES##|$LOG_DIR|g" config.py
+sed -i "s|##PATH##TO##INCONSISTENCY##FILES##|$REPORT_BASE_DIR|g" config.py
+
+pytest test_suite_0.py test_suite_1.py test_suite_2.py
+
+#parse bias summary from log files
+cd .. || exit
+echo "repairer parse_bias_info"
+python parse_bias_info.py "$MODEL_DIR""/test_result/repairer/log_files" "$MODEL_DIR""/test_result/repairer/bias_info_files" "$SAMPLING"
+echo "repairer summary result"
+python summary_result.py "$MODEL_DIR" $TEST_COUNT repairer
+echo "repairer count bias"
+python count_bias.py "$MODEL_DIR" $TEST_COUNT repairer
+echo "repairer count related"
+python count_related.py "$MODEL_DIR" $TEST_COUNT repairer
+echo "repairer count bias leaning"
+python count_bias_leaning.py "$MODEL_DIR" $TEST_COUNT repairer
